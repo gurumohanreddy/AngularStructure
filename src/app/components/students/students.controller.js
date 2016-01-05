5f04b86bd9a78ac6511d1a76d5be7dacec2a9533
gurumohanreddy <gurumohan449@gmail.com>
2016-01-05 17:37:06 +0530

    Introduce http

--- src/app/components/students/students.controller.js
+++ src/app/components/students/students.controller.js
@@ -1,9 +1,26 @@
   angular
     .module('skoolforum')
-    .controller('StudentController',[function(){
+    .controller('StudentController',['$http',function($http){
                           var vm = this;
 
+                          appId ='OXJqhw8TXb4dgsQXw3uTo3IxYGdL1pj0qjV0g0vW';
+                          restId = 'TKEZxU6JOZAsTwlpbPhAnJlstvn938KtuzNhFDCU';
+
                         vm.addstudent = function(){
+                              $http({
+                                 method: 'POST',
+                                 url: 'https://api.parse.com/1/classes/Student',
+                                 headers: {'X-Parse-Application-Id': appId, 'X-Parse-REST-API-Key': restId,
+                                 'Content-Type': 'application/json'},
+                                 data: vm.newstudent
+
+                              }).success(function(data){
+                                 console.log(data);
+                                 vm.initialize();
+
+                              }).error(function(data){
+                                 console.log(data)
+                              });
 
                         }
                         vm.initialize = function(){
