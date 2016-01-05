f076d0d445c0bcc8939d8e2b43513eb7b4f9b206
gurumohanreddy <gurumohan449@gmail.com>
2016-01-05 18:42:20 +0530

    Introduce ngresource

--- src/app/components/commonservices/restfulresources.factories.js
+++ src/app/components/commonservices/restfulresources.factories.js
@@ -1,16 +1,21 @@
-// angular
-//   .module('skoolforum')
-//   .factory('StudentResource',['$resource',function($resource){
-//
-//
-//     //
-//     // return $resource('https://api.parse.com/1/classes/Student/:Student',{Student: "@Student"}, save: {
-//     //         method: 'POST',
-//     //         headers: {
-//     //            'X-Parse-Application-Id': 'OXJqhw8TXb4dgsQXw3uTo3IxYGdL1pj0qjV0g0vW',
-//     //            'X-Parse-REST-API-Key': 'TKEZxU6JOZAsTwlpbPhAnJlstvn938KtuzNhFDCU'
-//     //         }
-//     //     });
-//
-//
-//   }]);
+angular
+  .module('skoolforum')
+  .factory('StudentResource',['$resource',function($resource){
+
+
+
+    var data = $resource('https://api.parse.com/1/classes/Student/:id',{id:"@"}, {
+              save: {
+                  method: 'POST',
+                  headers: {
+                   'X-Parse-Application-Id': 'OXJqhw8TXb4dgsQXw3uTo3IxYGdL1pj0qjV0g0vW',
+                   'X-Parse-REST-API-Key': 'TKEZxU6JOZAsTwlpbPhAnJlstvn938KtuzNhFDCU'
+                  }
+        }});
+
+        return data;
+
+  }]);
+
+
+  
--- src/app/components/students/students.controller.js
+++ src/app/components/students/students.controller.js
@@ -1,26 +1,28 @@
   angular
     .module('skoolforum')
-    .controller('StudentController',['$http',function($http){
+    .controller('StudentController',['$http','StudentResource','AuthenticationService',function($http,StudentResource,AuthenticationService){
                           var vm = this;
 
                           appId ='OXJqhw8TXb4dgsQXw3uTo3IxYGdL1pj0qjV0g0vW';
                           restId = 'TKEZxU6JOZAsTwlpbPhAnJlstvn938KtuzNhFDCU';
 
                         vm.addstudent = function(){
-                              $http({
-                                 method: 'POST',
-                                 url: 'https://api.parse.com/1/classes/Student',
-                                 headers: {'X-Parse-Application-Id': appId, 'X-Parse-REST-API-Key': restId,
-                                 'Content-Type': 'application/json'},
-                                 data: vm.newstudent
-
-                              }).success(function(data){
-                                 console.log(data);
-                                 vm.initialize();
-
-                              }).error(function(data){
-                                 console.log(data)
-                              });
+                              // $http({
+                              //    method: 'POST',
+                              //    url: 'https://api.parse.com/1/classes/Student',
+                              //    headers: {'X-Parse-Application-Id': appId, 'X-Parse-REST-API-Key': restId,
+                              //    'Content-Type': 'application/json'},
+                              //    data: vm.newstudent
+                              //
+                              // }).success(function(data){
+                              //    console.log(data);
+                              //    vm.initialize();
+                              //
+                              // }).error(function(data){
+                              //    console.log(data)
+                              // });
+                              StudentResource.save(vm.newstudent);
+                              console.log("Saved");
 
                         }
                         vm.initialize = function(){
